8c06006f35038b7721c77e89c2def253cc6fe63b
ThiagoMR00781 <tiagoribas00781@gmail.com>
2024-02-03 22:05:39 -0300

    first commit

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './Accordion.css'
 import AccordionItem from './AccordionItem'
 
 const Accordion = ({ items }) => {
-    const [activeIndex, setActiveIndex] = useState(null)
+    const [openItem, setOpenItem] = useState(null);
 
-    const OnTitleClick = (index) => {
-        setActiveIndex(activeIndex === index ? null : index)
-  }
-    // // essa parte eu teria que colocar no return, tá funcionando, mas sem css
-    //     <div>
-    //         {items.map((item, index) => (
-    //             <div key={index}>
-    //                 <button onClick={() => OnTitleClick(index)}>
-    //                     {item.title}
-    //                 </button>
-    //                 {index === activeIndex && <div>{item.content}</div>}
-    //                 </div>
-    //             ))}
-    //     </div>
-    //
+    const toggleItem = (itemIndex) => {
+    setOpenItem(openItem === itemIndex ? null : itemIndex);
+  };
     return (
         <div className="accordion">
             <div>
              {items.map((item, index) => (
                     <AccordionItem 
-                     key={item.id}
+                     key={item}
+                     index={index}
                      title={item.title}
-                     open={() => OnTitleClick}
                      content={item.content}
-                     isAcitive={activeIndex === index}
+                     isOpen={openItem === index}
+                     toggleItem={toggleItem}             
                     />
                 ))}
             </div>
--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-const AccordionItem = ({ title, content, open, isAcitive}) => {
+const AccordionItem = ({index, title, content, isOpen, toggleItem}) => {
   return (
-    <div className={`accordion-item ${open ? 'open' : ''}`} onClick={open}>
-        <header>{title}</header>
-        {isAcitive && <div className="accordion-item_body">{content}</div>}
+    <div className={`accordion-item ${isOpen ? 'open' : ''}`} onClick={() => toggleItem(index)}>
+      <header >{title}</header>
+      <div className="accordion-item_body">{content}</div>
     </div>
   )
 }
-
 export default AccordionItem
